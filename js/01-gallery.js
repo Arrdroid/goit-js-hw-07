2e3244975486133a5f3a907a2691f2f9930d03be
Arrdroid <alexmirensko@gmail.com>
2023-07-21 19:16:23 +0300

    summary

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,47 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const photoParent = document.querySelector("ul.gallery")
+
+const photoCollection = galleryItems.map(({ preview, original, description }) => {
+  return ` <li class="gallery__item">
+  <a class="gallery__link" href="${preview}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li> `
+}).join("");
+
+photoParent.insertAdjacentHTML("beforeend", photoCollection);
+
+// Delegation Listener to the parent - UL
+photoParent.addEventListener("click", onClickFn);
+
+// callback Function while clicking on image
+function onClickFn(event) {
+  event.preventDefault();
+
+  console.dir(event.target.nodeName);
+  if (event.target.nodeName !== "IMG") { return }
+  
+  const instance = basicLightbox.create(`
+    <img src="${event.target.dataset.source}" width="800" height="600">
+    `)
+
+  instance.show();
+  
+  photoParent.addEventListener("keydown", (event) => {
+    if (event.code === "Escape") {
+      instance.close();
+    }
+  });
+
+  photoParent.removeEventListener("click", onClickFn);
+}
+
+
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,20 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const photoParent = document.querySelector("ul.gallery")
 
-console.log(galleryItems);
+const photoCollection = galleryItems.map(({ preview, original, description }) => {
+  return  ` <li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li> `
+}).join("");
+
+photoParent.insertAdjacentHTML("beforeend", photoCollection);
+
+
+const photoPlugin = new SimpleLightbox(".gallery a", {
+  captions: true,
+  captionsData: "alt",
+  captionDelay: 250,
+});
